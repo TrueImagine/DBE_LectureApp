9b6e8e607eec09ee0a3e3bae8db489447de8b017
TrueImagine <imagin3.999@gmail.com>
2016-04-20 14:40:46 +0700

    DB update, homepage dosen mahasiswa edit menu

--- Tampilan/temp/mahasiswa.php
+++ Tampilan/temp/mahasiswa.php
@@ -164,21 +164,10 @@
                             <!-- /input-group -->
                         </li>
                         <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                            <a href="mahasiswa.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Kelas<span class="fa arrow"></span></a>
-							<ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">Tentang Kelas</a>
-                                </li>
-                                <li>
-                                    <a href="#">Materi</a>
-                                </li>
-								<li>
-									<a href="#">Tugas</a>
-								</li>
-                            </ul>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Kelas</a>
                             <!-- /.nav-second-level -->
 						</li>
                         
