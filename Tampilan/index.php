8e99c9e7e881128d0893d838cdbe54ed8220a553
32130012 <fedricoblaze@gmailcom>
2016-04-20 12:43:23 +0700

    homepage login
    homepage login with css

--- Tampilan/index.php
+++ Tampilan/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Lens Photography</title>
+		<title>LectureApps</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main.css" />
@@ -10,9 +10,10 @@
 	<body class="is-loading-0 is-loading-1 is-loading-2">
 		<!-- Main -->
 			<div id="banner">
-				Lorem ipsum dolor sit amet
+				<h1>ASISTEN DOSEN</h1>
 			</div>
-			<div id="container">
+			<div id="viewer">
+			<div id="badan">
 				<img src="images/fulls/LOGO BIOS.jpeg" />
 				<div>
 					<h1>Selamat Datang di LectureApp</h1>
@@ -22,29 +23,19 @@
 					Daftar untuk menjadi pengajar, buat kelas Anda dan daftarkan murid-murid Anda untuk menjadi bagian dari
 					pembelajaran yang modern!
 				</p>
-				
+			</div>
+				SILAHKAN ISI
 			</div>
 			<div id="main">
 				<!-- Header -->
 					<header id="header">
 						<section class="login">
-							<div class="titulo">Login Pengajar</div>
+							<div class="titulo">Login LecturerApps</div>
 								<form action="#" method="post" enctype="application/x-www-form-urlencoded">
 									<input type="text" required title="Username required" placeholder="Email" data-icon="U">
 									<input type="password" required title="Password required" placeholder="Password" data-icon="x">
 									<div class="olvido">
 										<div class="col"><a href="#" title="Ver Carásteres">Register</a></div>
-										<div class="col"><a href="#" title="Recuperar Password">Forgot Password?</a></div>
-									</div>
-									<a href="#" class="enviar">Login</a>
-								</form>
-						</section>
-						<section class="login">
-							<div class="titulo">Login Peserta Ajar</div>
-								<form action="#" method="post" enctype="application/x-www-form-urlencoded">
-									<input type="text" required title="Username required" placeholder="Email" data-icon="U">
-									<input type="password" required title="Password required" placeholder="Password" data-icon="x">
-									<div class="olvido">
 										<div class="col"><a href="#" title="Recuperar Password">Forgot Password?</a></div>
 									</div>
 									<a href="#" class="enviar">Login</a>
@@ -67,7 +58,7 @@
 				<!-- Footer -->
 					<footer id="footer">
 						<ul class="copyright">
-							<li>&copy; 2016 Assisten Dosen</li>
+							<li>&copy; LectureApps - 2016</li>
 						</ul>
 					</footer>
 
