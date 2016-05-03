a9e9a7b47a57781d2839e45390f0b57eefba466f
TrueImagine <imagin3.999@gmail.com>
2016-05-04 00:36:11 +0700

    Mahasiswa Edit

--- Tampilan/includes/mhs_dtlmenu.php
+++ Tampilan/includes/mhs_dtlmenu.php
@@ -1,5 +1,5 @@
-<a href="kelas_mhs_detail.php?idkelas=<?php echo $id; ?>">Tentang Kelas</a> | 
-<a href="kelas_mhs_materi.php?idkelas=<?php echo $id; ?>">Materi</a> | 
-<a href="kelas_mhs_tugas.php?idkelas=<?php echo $id; ?>">Tugas</a> 
+<a href="kelas_mhs_detail.php?idKelas=<?php echo $id; ?>">Tentang Kelas</a> | 
+<a href="kelas_mhs_materi.php?idKelas=<?php echo $id; ?>">Materi</a> | 
+<a href="kelas_mhs_tugas.php?idKelas=<?php echo $id; ?>">Tugas</a>
 
-</br></br>
+<br />
